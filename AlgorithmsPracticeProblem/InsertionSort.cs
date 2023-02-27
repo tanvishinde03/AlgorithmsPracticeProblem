687a31614f3e951f47b2f493de5b7603cf961620
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-27 22:13:39 +0530

    [Tanvi] Add : UC3 - Insertion Sort

---
+++ AlgorithmsPracticeProblem/InsertionSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPracticeProblem
+{
+    public class InsertionSort
+    {
+        public void Sort()
+        {
+            int[] arr =  { 8, 5, 7, 3, 1, 6, 2, 4, 9, 0 };
+            int n = 10, i, j, val;
+            Console.WriteLine("Insertion Sort");
+            Console.Write(" array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                for (j = i - 1; j >= 0;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.Write("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+    }
+}
---
+++ AlgorithmsPracticeProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Option");
+            Console.WriteLine("1.Insertion Sort");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    InsertionSort sort = new InsertionSort();
+                    sort.Sort();
+                    break;
+
+
+
+
+
+            }
+        }
+    }
+}
